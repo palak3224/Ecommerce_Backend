6a206cfc1f66c74af77b3cb3bf6cb92e84546971
Nihal <nihalpuse1234@gmail.com>
2025-06-16 16:29:01 +0530

    Added Backend for Newsletter Subscription

---
+++ models/newsletter_subscription.py
@@ -0,0 +1,8 @@
+from common.database import db
+
+class Subscription(db.Model):
+    __tablename__ = 'subscriptions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    created_at = db.Column(db.DateTime, nullable=False)
